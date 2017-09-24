d714b98ed7a4fd7ab03bebb3e12997ecbba8d185
Jacques Michiels <jacques@smartmakers.de>
2017-09-24 14:18:33 +0200

    refactored arm_control

--- src/arm_control/src/joint_trajectory_controller.cpp
+++ src/arm_control/src/joint_trajectory_controller.cpp
@@ -1,4 +1,11 @@
-#include <arm_control/joint_trajectory_controller.h>
+#include <arm_control/effort_joint_interface.h>
 #include <pluginlib/class_list_macros.h>
+
+namespace arm_control
+{
+    typedef joint_trajectory_controller::JointTrajectoryController<trajectory_interface::QuinticSplineSegment<double>,
+            hardware_interface::EffortJointInterface> JointTrajectoryController;
+
+} // namespace
 
 PLUGINLIB_EXPORT_CLASS(arm_control::JointTrajectoryController, controller_interface::ControllerBase);
